a44b1c22ccd60fd7249455276a28024b1437ddd0
anup09 <anup.vajareenkar@gmail.com>
2025-08-08 01:55:45 +0530

    keep common name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-angular-demo-project-by Anup';
+  title = 'my-angular-tested properly';
 }
